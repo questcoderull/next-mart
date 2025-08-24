3adcc6a4c4b711a421200a648086e9b0eecb2a7e
questcoderull <questcoderull@gmail.com>
2025-08-24 19:59:11 +0600

    next.js basic set up

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="max-w-7xl">
+      <nav>
+        <ul className="flex justify-between">
+          <li>Home</li>
+          <li>Product</li>
+          <li>About</li>
+          <li>More..</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
